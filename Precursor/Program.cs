c349d1728514671b95a20665f398bc93132cea9f
MtnDewIt <107611247+MtnDewIt@users.noreply.github.com>
2025-05-05 22:42:42 +1000

    Add Basic Dependency Tests
    It can't find TagTool or HaloShaderGenerator for some reason

--- Precursor/Program.cs
+++ Precursor/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Precursor
+﻿using System.Globalization;
+using TagTool.Commands.Common;
+using HaloShaderGenerator.Globals;
+
+namespace Precursor
 {
     public static class Program
     {
         static void Main(string[] args)
         {
-            
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.GetCultureInfo("en-US");
+
+            var hlslFloat = HLSLType.Float;
+            new TagToolWarning("Silence fills the empty grave");
+
+            // Basic idea is it opens its own command context similar to TagTool
+            // The input file is a static json file containing all paths to all required engine versions
+            // all unit tests are run in parallel with the option to run unit tests for a given cache version, or cache generation
+            // shader unit tests are also run in parallel, with separate tests for explicit, chud, global, and template generation
+            // default config is to run all unit tests available.
         }
     }
 }
