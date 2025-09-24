fd2fb54d623a90b8753535c0d56fb8982e123f3a
MtnDewIt <107611247+MtnDewIt@users.noreply.github.com>
2025-09-24 14:23:41 +1000

    Update Project Configuration
    Everything is now in a single path

--- Precursor/App.xaml.cs
+++ Precursor/App.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.Loader;
+using System.Windows;
 
 namespace Precursor
 {
@@ -9,7 +14,39 @@ namespace Precursor
     {
         static App() 
         {
-            
+            string searchPath = Path.Combine(AppContext.BaseDirectory, "Tools");
+
+            AssemblyLoadContext.Default.Resolving += (AssemblyLoadContext ctx, AssemblyName name) =>
+            {
+                try
+                {
+                    string assemblyPath = Path.Combine(searchPath, $"{name.Name}.dll");
+                    if (File.Exists(assemblyPath))
+                        return AssemblyLoadContext.Default.LoadFromAssemblyPath(assemblyPath);
+                }
+                catch (Exception)
+                {
+                    // swallow
+                }
+
+                return null;
+            };
+
+            AssemblyLoadContext.Default.ResolvingUnmanagedDll += (Assembly assembly, string dllName) =>
+            {
+                try
+                {
+                    string dllPath = Path.Combine(searchPath, dllName);
+                    if (File.Exists(dllPath))
+                        return NativeLibrary.Load(dllPath);
+                }
+                catch (Exception)
+                {
+                    // swallow
+                }
+
+                return IntPtr.Zero;
+            };
         }
 
         protected override void OnStartup(StartupEventArgs e)
